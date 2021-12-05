ef67658564898680863777c54b69e04d4d97f7e7
HelenKoppel <81012100+HelenKoppel@users.noreply.github.com>
2021-12-05 13:13:57 +0200

    [add] - category endpoint for data creation in progress

--- src/main/java/com/example/endofgame/dto/CategorySummary.java
+++ src/main/java/com/example/endofgame/dto/CategorySummary.java
@@ -3,6 +3,7 @@ package com.example.endofgame.dto;
 import java.time.LocalDateTime;
 
 public record CategorySummary(Long id, String name,
+                              // ToDo: change format of those two fields for nicer one
                               LocalDateTime created,
                               LocalDateTime updated) {
 }
